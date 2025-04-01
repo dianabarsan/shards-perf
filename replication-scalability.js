f36cb39a37cec4365a96d70738f1439556c7baa0
Diana Barsan <barsan@medic.org>
2025-04-01 19:43:58 +0300

    add cluster compose

--- replication-scalability.js
+++ replication-scalability.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 const { request } = require('./utils');
 const fs = require('fs');
 
-const NBR = 50;
+const NBR = 100;
 
 const getUsers = async () => {
   const users = await request({ uri: `_users/_all_docs?limit=${NBR}&start_key="org.couchdb.user:u"` });
