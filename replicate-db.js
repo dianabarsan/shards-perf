c038529ef9286253c82d20a717edc374c2851268
Diana Barsan <barsan@medic.org>
2025-04-02 13:00:50 +0300

    add cluster compose

--- replicate-db.js
+++ replicate-db.js
@@ -1,13 +1,12 @@
 const [,, db1, db2] = process.argv;
 const PouchDB = require('pouchdb');
-const { request, password } = require('./utils');
 
-const remoteDb1 = new PouchDB(`http://localhost:5988/${db1}`, {
-  auth: { username: 'admin', password: 'pass' },
-});
+const dbName1 = db1.startsWith('http') ? db1 : `http://localhost:5988/${db1}`;
+const dbName2 = db2.startsWith('http') ? db2 : `http://localhost:5988/${db2}`;
 
-const remoteDb2 = new PouchDB(`http://localhost:5988/${db2}`, {
-  auth: { username: 'admin', password: 'pass' },
-});
+const opts = { auth: { username: 'admin', password: 'pass' } };
+
+const remoteDb1 = new PouchDB(dbName1, opts);
+const remoteDb2 = new PouchDB(dbName2, opts);
 
 remoteDb1.replicate.to(remoteDb2, { batch_size: 1000 });
